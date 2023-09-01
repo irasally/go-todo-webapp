bef79b37f40c61f876789ccbb4982580436ce1c4
irasally <irasally@gmail.com>
2023-09-01 13:53:35 +0900

    DBに接続しテーブルを作成する処理

--- main.go
+++ main.go
@@ -19,6 +19,25 @@ type Todo struct {
 }
 
 func main() {
+	sqldb, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqldb.Close()
+
+	db := bun.NewDB(sqldb, pgdialect.New())
+	defer db.Close()
+
+	db.AddQueryHook(bundebug.NewQueryHook(
+		//bundebug.WithVerbose(true),
+		bundebug.FromEnv("BUNDEBUG"),
+	))
+
+	ctx := context.Background()
+	_, err = db.NewCreateTable().Model((*Todo)(nil)).ifNotExists().Exec(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 	e := echo.New()
 	e.get("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "")
