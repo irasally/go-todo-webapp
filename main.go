cc6a2e0be2ffc288c3446d690888df542a7e54af
irasally <irasally@gmail.com>
2023-09-01 18:13:06 +0900

    API側のCRUDを実装した
    mainに全部書く あとからなんとかしたい

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"log"
 	"net/http"
 	"os"
 	"time"
 
-	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
+
+	"github.com/labstack/echo/v4"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/pgdialect"
 	"github.com/uptrace/bun/extra/bundebug"
@@ -25,6 +27,25 @@ type Todo struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time `bun:",nullzero"`
 	DeletedAt time.Time `bun:",soft_delete,nullzero"`
+}
+
+type Data struct {
+	Todos []Todo
+	Errors []error
+}
+
+func customFunc(todo *Todo) func([]string) []error{
+	return func(values []string) []error {
+		if len(values) == 0 || values[0] == "" {
+			return nil
+		}
+		dt, err := time.Parse("2006-01-02T15:04 MST", values[0]+" JST")
+		if err != nil {
+			return []error{err}
+		}
+		todo.Until = dt
+		return nil
+	}
 }
 
 func main() {
@@ -50,7 +71,64 @@ func main() {
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "")
+		var todos []Todo
+		ctx := context.Background()
+		err := db.NewSelect().Model(&todos).Order("created_at").Scan(ctx)
+		if err != nil {
+			e.Logger.Error(err)
+			return c.Render(http.StatusBadRequest, "index", Data{
+				Errors: []error{errors.New("Cannot get todos")},
+			})
+		}
+		return c.Render(http.StatusOK, "index", Data{ Todos: todos })
+	})
+	e.POST("/", func(c echo.Context) error {
+		var todo Todo
+		// パラメーターをフィールドにバインド
+		errs := echo.FormFieldBinder(c).
+		Int64("id", &todo.ID).
+		String("content", &todo.Content).
+		Bool("done", &todo.Done).
+		CustomFunc("until", customFunc(&todo)).
+		BindErrors()
+		if errs != nil {
+			e.Logger.Error(err)
+			return c.Render(http.StatusBadRequest, "index", Data{Errors: errs})
+		} else if todo.ID == 0 {
+			// IDが0の特は登録
+			ctx := context.Background()
+			if todo.Content == "" {
+				err = errors.New("Content is empty")
+			} else {
+				_, err = db.NewInsert().Model(&todo).Exec(ctx)
+				if err != nil {
+					e.Logger.Error(err)
+					err = errors.New("Cannot insert todo")
+				}
+			}
+		} else {
+			ctx := context.Background()
+			if c.FormValue("delete") != "" {
+				// 削除
+				_, err = db.NewDelete().Model(&todo).Where("id = ?", todo.ID).Exec(ctx)
+			} else {
+				// 更新
+				var orig Todo
+				err = db.NewSelect().Model(&orig).Where("id = ?", todo.ID).Scan(ctx)
+				if err == nil {
+					orig.Done = todo.Done
+					_, err = db.NewUpdate().Model(&orig).Where("id = ?", todo.ID).Exec(ctx)
+				}
+			}
+			if err != nil {
+				e.Logger.Error(err)
+				err = errors.New("Cannot update todo")
+			}
+		}
+		if err != nil {
+			return c.Render(http.StatusBadRequest, "index", Data{Errors: []error{err}})
+		}
+		return c.Redirect(http.StatusFound, "/")
 	})
 	e.Logger.Fatal(e.Start(":8989"))
 }
