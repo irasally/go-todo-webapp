4c45e558e3d8df115cb485dcd2ffa725b7426f85
irasally <irasally@gmail.com>
2023-09-01 13:50:12 +0900

    TODOモデルの実装

--- main.go
+++ main.go
@@ -6,6 +6,18 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Todo struct {
+	bun.BaseModel `bun:"table:todos,alias:t"`
+
+	ID int64 `bun:"id,pk,autoincrement"`
+	Content string `bun:"content,notnull"`
+	Done bool `bun:"done"`
+	Until time.Time `bun:"until,nullzero"`
+	CreatedAt time.Time
+	UpdatedAt time.Time `bun:",nullzero"`
+	DeletedAt time.Time `bun:",soft_delete,nullzero"`
+}
+
 func main() {
 	e := echo.New()
 	e.get("/", func(c echo.Context) error {
