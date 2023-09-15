68d870bd2e75f847d8664239d331c815478c59d2
irasally <irasally@gmail.com>
2023-09-15 18:13:37 +0900

    変数に入れたから返さなくて良いかな

--- model/db.go
+++ model/db.go
@@ -13,7 +13,7 @@ import (
 
 var db *bun.DB
 
-func DBConnection() *bun.DB {
+func DBConnection(){
 	sqldb, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatal(err)
