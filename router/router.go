05252c67450e12e90fd2764e57c93b78954c49f7
irasally <irasally@gmail.com>
2023-09-22 17:51:46 +0900

    routerは薄く、routingごとの定義を別ファイルにした

--- router/router.go
+++ router/router.go
@@ -1,19 +1,10 @@
 package router
 
 import (
-	"errors"
-	"net/http"
-
-	"todo-webapp/model"
-
 	"github.com/labstack/echo/v4"
 )
 
 var logger echo.Logger
-type Data struct {
-	Todos []model.Todo
-	Errors []error
-}
 
 func setupLogger(e *echo.Echo){
 	logger = e.Logger
@@ -22,72 +13,8 @@ func setupLogger(e *echo.Echo){
 func SetRouter(e *echo.Echo) {
 	setupLogger(e)
 
-	e.GET("/", func(c echo.Context) error {
-		todos, err := model.GetTodos()
-
-		if err != nil {
-			e.Logger.Error(err)
-			return c.Render(http.StatusBadRequest, "index", Data{
-				Errors: []error{errors.New("Cannot get todos")},
-			})
-		}
-		return c.Render(http.StatusOK, "index", Data{ Todos: todos })
-	})
-
-	e.POST("/", func(c echo.Context) error {
-		var todo model.Todo
-		var err error
-
-		// パラメーターをフィールドにバインド
-		errs := echo.FormFieldBinder(c).
-									Int64("id", &todo.ID).
-									String("content", &todo.Content).
-									Bool("done", &todo.Done).
-									CustomFunc("until", customFunc(&todo)).
-									BindErrors()
-
-		if errs != nil {
-			e.Logger.Error(errs)
-			return c.Render(http.StatusBadRequest, "index", Data{Errors: errs})
-		} else if todo.ID == 0 {
-			err = add(todo)
-		} else {
-			if c.FormValue("delete") != "" {
-				err = delete(todo)
-			} else {
-				err = doneTodo(todo)
-			}
-		}
-		if err != nil {
-			return c.Render(http.StatusBadRequest, "index", Data{Errors: []error{err}})
-		}
-		return c.Redirect(http.StatusFound, "/")
-	})
-}
-
-func add(todo model.Todo) error {
-	err := model.AddTodo(todo)
-	if err != nil {
-		logger.Error(err)
-		err = errors.New("Cannot insert todo")
-	}
-	return err
-}
-
-func delete(todo model.Todo) error{
-	err := model.DeleteTodo(todo)
-	if err != nil {
-		logger.Error(err)
-		err = errors.New("Cannot delete todo")
-	}
-	return err
-}
-
-func doneTodo(todo model.Todo) error {
-	err := model.UpdateTodo(todo)
-	if err != nil {
-		logger.Error(err)
-		err = errors.New("Cannot update todo")
-	}
-	return err
+	// GET "/"
+	root_get(e)
+	// POST "/"
+	root_post(e)
 }
---
+++ router/todo.go
@@ -0,0 +1,87 @@
+package router
+
+import (
+	"errors"
+	"net/http"
+	"todo-webapp/model"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Data struct {
+	Todos []model.Todo
+	Errors []error
+}
+
+func root_get(e *echo.Echo){
+	e.GET("/", func(c echo.Context) error {
+		todos, err := model.GetTodos()
+
+		if err != nil {
+			e.Logger.Error(err)
+			return c.Render(http.StatusBadRequest, "index", Data{
+				Errors: []error{errors.New("Cannot get todos")},
+			})
+		}
+		return c.Render(http.StatusOK, "index", Data{ Todos: todos })
+	})
+}
+
+func root_post(e *echo.Echo) {
+	e.POST("/", func(c echo.Context) error {
+		var todo model.Todo
+		var err error
+
+		// パラメーターをフィールドにバインド
+		errs := echo.FormFieldBinder(c).
+									Int64("id", &todo.ID).
+									String("content", &todo.Content).
+									Bool("done", &todo.Done).
+									CustomFunc("until", customFunc(&todo)).
+									BindErrors()
+
+		if errs != nil {
+			e.Logger.Error(errs)
+			return c.Render(http.StatusBadRequest, "index", Data{Errors: errs})
+		} else if todo.ID == 0 {
+			err = add(todo)
+		} else {
+			if c.FormValue("delete") != "" {
+				err = delete(todo)
+			} else {
+				err = doneTodo(todo)
+			}
+		}
+		if err != nil {
+			return c.Render(http.StatusBadRequest, "index", Data{Errors: []error{err}})
+		}
+		return c.Redirect(http.StatusFound, "/")
+	})
+}
+
+func add(todo model.Todo) error {
+	err := model.AddTodo(todo)
+	if err != nil {
+		logger.Error(err)
+		err = errors.New("Cannot insert todo")
+	}
+	return err
+}
+
+func delete(todo model.Todo) error{
+	err := model.DeleteTodo(todo)
+	if err != nil {
+		logger.Error(err)
+		err = errors.New("Cannot delete todo")
+	}
+	return err
+}
+
+func doneTodo(todo model.Todo) error {
+	err := model.UpdateTodo(todo)
+	if err != nil {
+		logger.Error(err)
+		err = errors.New("Cannot update todo")
+	}
+	return err
+}
